9e61993d3616d36cf209cf84d2249921bdb83e3f
akira noguchi <guppy0356.nakira@gmail.com>
2019-03-27 13:53:28 +0900

    Add counter feature

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <div id="app">
+    <h1>{{ $store.state.count }}</h1>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+    },
+    decrement() {
+      this.$store.commit('decrement')
+    },
+  }
 }
 </script>
